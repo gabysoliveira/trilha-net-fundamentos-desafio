fd4295b694896341cb190a4ef0eaf9a045b4664f
Gabriele Oliveira <gabrieleoliveira7846@gmail.com>
2024-01-10 16:56:20 -0300

    Finalizado

--- DesafioFundamentos/Program.cs
+++ DesafioFundamentos/Program.cs
@@ -27,7 +27,8 @@ while (exibirMenu)
     Console.WriteLine("1 - Cadastrar veículo");
     Console.WriteLine("2 - Remover veículo");
     Console.WriteLine("3 - Listar veículos");
-    Console.WriteLine("4 - Encerrar");
+    Console.WriteLine("4 - Exibir Horários");
+    Console.WriteLine("5 - Encerrar");
 
     switch (Console.ReadLine())
     {
@@ -44,6 +45,10 @@ while (exibirMenu)
             break;
 
         case "4":
+            es.ExibirRegistros();
+            break;
+
+        case "5":
             exibirMenu = false;
             break;
 
